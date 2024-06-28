d71c732a9723ddce983519f465bc33185ddb1768
llssbb <bt.santosh01@gmail.com>
2024-06-28 22:38:02 +0100

    added tests

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 from ..database import Base
@@ -6,6 +6,8 @@ from ..main import app
 from ..routers.todos import get_db, get_current_user
 from fastapi.testclient import TestClient
 from fastapi import status
+import pytest
+from ..models import Todos
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
@@ -34,7 +36,38 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 client = TestClient(app)
 
-def test_read_all_authenticated():
+@pytest.fixture
+def test_todo():
+    todo = Todos(
+        title="Learn to code!",
+        description="Need to learn everyday!",
+        priority=5,
+        complete=False,
+        owner_id=1
+    )
+
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("Delete from todos;"))
+        connection.commit()
+
+def test_read_all_authenticated(test_todo):
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == []
+    assert response.json() == [{ 'id': 1, 'complete': False, 'title': 'Learn to code!', 'description': 'Need to learn everyday!',
+                                'owner_id': 1, 'priority': 5}]
+
+
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == { 'id': 1, 'complete': False, 'title': 'Learn to code!', 'description': 'Need to learn everyday!',
+                                'owner_id': 1, 'priority': 5}
+
+def test_read_one_authenticated_not_found():
+    response = client.get("/todo/999")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found.'}
